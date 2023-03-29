7becebb5ea8a8566b6392b3e7eb1826144319d99
leeduy1 <levanduy.gv2003@gmail.com>
2023-03-29 15:26:34 +0700

    feat: ui bottom navigation

---
+++ src/components/modules/ProfileRoute.tsx
@@ -0,0 +1,5 @@
+import { Text } from "react-native-paper";
+
+export default function ProfileRoute() {
+  return <Text>ProfileRoute</Text>;
+}
---
+++ src/screens/Dashboard.tsx
@@ -0,0 +1,61 @@
+import * as React from "react";
+import { BottomNavigation, Text } from "react-native-paper";
+import ChatsRoute from "../components/modules/ChatsRoute";
+import ContactsRoute from "../components/modules/ContactsRoute";
+import GroupsRoute from "../components/modules/GroupsRoute";
+import ProfileRoute from "../components/modules/ProfileRoute";
+import SettingsRoute from "../components/modules/SettingsRoute";
+
+const Dashboard = () => {
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    {
+      key: "profile",
+      title: "Profile",
+      focusedIcon: "account",
+      unfocusedIcon: "account-outline",
+    },
+    {
+      key: "chats",
+      title: "Chats",
+      focusedIcon: "chat-processing",
+      unfocusedIcon: "chat-processing-outline",
+    },
+    {
+      key: "groups",
+      title: "Groups",
+      focusedIcon: "account-multiple",
+      unfocusedIcon: "account-multiple-outline",
+    },
+    {
+      key: "contacts",
+      title: "Contacts",
+      focusedIcon: "contacts",
+      unfocusedIcon: "contacts-outline",
+    },
+    {
+      key: "settings",
+      title: "Settings",
+      focusedIcon: "cog",
+      unfocusedIcon: "cog-outline",
+    },
+  ]);
+
+  const renderScene = BottomNavigation.SceneMap({
+    profile: ProfileRoute,
+    chats: ChatsRoute,
+    groups: GroupsRoute,
+    contacts: ContactsRoute,
+    settings: SettingsRoute,
+  });
+
+  return (
+    <BottomNavigation
+      navigationState={{ index, routes }}
+      onIndexChange={setIndex}
+      renderScene={renderScene}
+    />
+  );
+};
+
+export default Dashboard;
